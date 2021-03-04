be16afcc4a78e5cb063a29321e450446ccd2e163
IaraSelva <71565887+IaraSelva@users.noreply.github.com>
2021-03-04 19:24:26 -0300

    inserção da sub Postos

---
+++ projetoMod1/testePostos.java
@@ -0,0 +1,17 @@
+package projetoMod1;
+
+public class testePostos {
+
+	public static void main(String[] args) {
+
+		Postos posto1 = new Postos(20,10,"ZN");
+		
+		posto1.escolhaPosto("ZS");
+		posto1.escolhaPosto("ZN");
+		posto1.escolhaPosto("ZN");
+		posto1.escolhaPosto("ZO");
+		posto1.escolhaPosto("ZL");
+		
+	}
+
+}
