31e4fe8f6982da2185210f79dd1be13172ebf804
IaraSelva <71565887+IaraSelva@users.noreply.github.com>
2021-03-07 23:16:52 -0300

    exercicio melhorado

--- Collections/Estoque.java
+++ Collections/Estoque.java
@@ -7,19 +7,30 @@ import java.util.Scanner;
 public class Estoque {
 	public static void main (String [] args) {
 		
+				
 		ArrayList <String> Estoque = new ArrayList <String>();
 		Scanner leia = new Scanner(System.in);
 		
-		do {
-			System.out.println("Entre com o produto que deseja adcionar ao estoque: (\nOu 0 para sair.)");
+		System.out.println("Quantos produtos gostaria de adcionar ao estoque?");
+		int add = leia.nextInt();
+		
+		for(int i=0; i<add; i++)
+		{	leia.nextLine();
+			//produto = estoque[0]
+			System.out.println("Entre com o produto "+(i+1)+" que deseja adcionar ao estoque: ");
 			String produto = leia.nextLine();
 			Estoque.add(produto);
-			System.out.println(Estoque);
-		}while(!=0);
-		
-		
 
-			
+		}
+		
+		for(String produtos:Estoque) {
+			System.out.println(produtos);
+		}
+		
+		Estoque.remove(0);
+		
+		System.out.println("Estoque atualizado: "+Estoque);
+		
 	}
 
 }
