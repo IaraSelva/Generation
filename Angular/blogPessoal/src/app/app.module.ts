4d4c05bc56dccc7207d9a7fa1113d7c3c8ebf204
iara <iaiaselva.is@gmail.com>
2021-04-27 14:40:25 -0300

    implementação páginas login e cadastro

--- Angular/blogPessoal/src/app/app.module.ts
+++ Angular/blogPessoal/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
