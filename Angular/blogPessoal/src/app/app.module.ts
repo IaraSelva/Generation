44b96e29e43a8ab5c7bfd220d7ee8b248decb821
iara <iaiaselva.is@gmail.com>
2021-05-03 09:24:05 -0300

    criação de model e service

--- Angular/blogPessoal/src/app/app.module.ts
+++ Angular/blogPessoal/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +22,9 @@ import { SubscribeComponent } from './subscribe/subscribe.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
